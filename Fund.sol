9479ae564190b0fa3575ac1f28ac908b405c5878
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2023-08-24 12:31:11 +0500

    Updated

--- Fund.sol
+++ Fund.sol
@@ -3,6 +3,25 @@ pragma solidity 0.8.21;
 
 contract CrowdFund {
 
-    
+    address public Manager;
+    mapping(address=>uint) public Contributors;
+
+    constructor() {
+        Manager=msg.sender;
+    }
+
+    struct Project{
+
+        string ProjectName;
+        string Description;
+        uint RequiredAmount;
+        uint Deadline;
+        uint MinimumContribution;
+        uint NoofContributors;
+        address payable recipient;
+
+    }
+
+
     
 }
