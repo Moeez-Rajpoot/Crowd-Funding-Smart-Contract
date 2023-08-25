b4ed0912247ba63dbae9ce9b4444638b7f91b2fa
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2023-08-25 10:32:44 +0500

    Updated

--- Fund.sol
+++ Fund.sol
@@ -5,6 +5,7 @@ contract CrowdFund {
 
     address public Manager;
     uint public Campaignno =0;
+    uint public RemainingCampaign =0;
     uint[] public CompletedCampaignID;
     mapping(uint=>Campaign) public CampaignLocation;
 
@@ -45,10 +46,12 @@ contract CrowdFund {
         newCampaign.MinimumContribution = _miniamount;
         newCampaign.recipient = _receipt;
         Campaignno++;
+        RemainingCampaign++;
     }
 
     function SendFund(uint _CampaignID) payable public  {
       Campaign storage OldCampaign =  CampaignLocation[_CampaignID];
+      require(RemainingCampaign > 0 ,"There is No Campaign In Process Yet");
       require(OldCampaign.MinimumContribution <= msg.value , "Minimum Contribution Requirement No met ");
       require(OldCampaign.Deadline > block.timestamp , "Opss The Time is Over For Contribution");
       require(OldCampaign.AmountCollected < OldCampaign.RequiredAmount ,"Requirement is Fullfiled For This Campaign" );
@@ -65,7 +68,7 @@ contract CrowdFund {
 
     function Refund(uint _CampaignID) public  {
         Campaign storage OldCampaign =  CampaignLocation[_CampaignID];
-        require(Campaignno > 0 ,"There is No Campaign In Process Yet");
+        require(RemainingCampaign > 0 ,"There is No Campaign In Process Yet");
         require(OldCampaign.Deadline < block.timestamp,"Campaign is Still in Process You Cant Refund Yet");
         require(OldCampaign.Contributors[msg.sender]>0,"You are not a Contributor");
         require(OldCampaign.AmountCollected < OldCampaign.RequiredAmount,"Required Campaign Amount has been Fulfilled Can't Refund Now");
@@ -80,14 +83,34 @@ contract CrowdFund {
 
     }
 
-    function checkbalance() public view returns (uint){
+    function VoteForTransfer(uint _CampaignID) public {
+        Campaign storage OldCampaign =  CampaignLocation[_CampaignID];
+        require(RemainingCampaign > 0 ,"There is No Campaign In Process Yet");
+        require(OldCampaign.Contributors[msg.sender]>0 ,"You are not a Contributor");
+        require(OldCampaign.AmountCollected >= OldCampaign.RequiredAmount ,"Can't Vote The Requirement is not FullFilled Yet ");
+        require(OldCampaign.Deadline < block.timestamp,"Campaign is Still in Process You Cant Refund Yet");
+        require(OldCampaign.Completed == false , "The Campaign has been Completed You Cant Refund Now");
+        require(OldCampaign.Vote[msg.sender]==false , "You Already Have Voted");
 
-        return address(this).balance;
+        OldCampaign.Vote[msg.sender]=true;
+        OldCampaign.NoofVotes++;
     }
 
-    
+    function MakePayment(uint _CampaignID) public OnlyManager {
+        Campaign storage OldCampaign =  CampaignLocation[_CampaignID];
+        require(RemainingCampaign > 0 ,"There is No Campaign In Process Yet");
+        require(OldCampaign.Deadline < block.timestamp,"Campaign is Still in Process");
+        require(OldCampaign.AmountCollected >= OldCampaign.RequiredAmount ,"The Requirement is not FullFilled Yet");
+        require(OldCampaign.NoofVotes > OldCampaign.NoofContributors/2 ," Not Enough Vote to Make Payment");
+        
+        OldCampaign.recipient.transfer(OldCampaign.RequiredAmount);
+        OldCampaign.Completed=true;
+        RemainingCampaign--;
 
+    }
 
+    function Check_Contract_balance() public view returns (uint){
 
-    
+        return address(this).balance;
+    }    
 }
